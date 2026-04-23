0d9d409bf317cfead44932acfb4f39350c8ad0e6
tsabale-20 <tejashree.sabale@sumasoft.net>
2026-04-23 17:21:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,9 @@ if ingredients_list:
       session.sql(my_insert_stmt).collect()
       st.success('Your Smoothie is ordered!', icon="✅")
 
-  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+# st.text(smoothiefroot_response.json())    
+sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)  
 
 
     
