dd980fc8b74cfd39a76b9ed8d7a0f82db143d2db
tsabale-20 <tejashree.sabale@sumasoft.net>
2026-04-23 17:18:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages.
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app.
 st.title(f"Customer smoothie :cup_with_straw:")
@@ -42,10 +43,9 @@ if ingredients_list:
       session.sql(my_insert_stmt).collect()
       st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests  
+  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response.json())    
-#sf_df=sf.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
     
   
