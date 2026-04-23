69afa9a178e577c597ada8a98b9fd7fe9a85d091
tsabale-20 <tejashree.sabale@sumasoft.net>
2026-04-23 17:16:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ if ingredients_list:
 
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-#st.text(smoothiefroot_response.json())    
-sf_df=sf.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+st.text(smoothiefroot_response.json())    
+#sf_df=sf.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     
   
