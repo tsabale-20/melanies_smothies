70d3d55511b339d051f544e74ae94b34f1ae71ac
tsabale-20 <tejashree.sabale@sumasoft.net>
2026-04-23 17:18:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,6 @@ if ingredients_list:
       st.success('Your Smoothie is ordered!', icon="✅")
 
   
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 
 
     
